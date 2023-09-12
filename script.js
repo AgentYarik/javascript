2c8cece16ea49653fa8539a38e667c760b8ac4c1
AgentYarik <125293714+AgentYarik@users.noreply.github.com>
2023-09-12 19:34:00 +0300

    js

---
+++ script.js
@@ -0,0 +1,93 @@
+/* let number = 23;
+let numberboxTwo = 47;
+let summ = number + numberboxTwo;
+console.log(summ)
+confirm(number)
+
+let myName = 'Yaroslav';
+let myAge = 12;
+let myAgeLetter = 12;
+alert(myAge + myAgeLetter)
+prompt(myName) */
+
+/* const GreenArrow = 'Oliver Queen' */
+/* let GreenArrow = 'Oliver Queen'
+var Team = 'Arrow'
+
+GreenArrow = 'Jond Digl'
+Team = 'ARGUS'
+
+console.log(GreenArrow + Team) */
+/* let yarikName = GreenArrow
+let yarik = false;
+let yarikAge = 12;
+console.log(typeof yarik) */
+
+/* let name  = "GreenArrowTeam";
+let group = "Я навчаюся в лигі вбивць";
+
+console.log( name + group ); */
+
+/* let myName = 'Yaroslav';
+let secondName = 'Rubanik'
+let myAge = 12;
+let year = 2010;
+
+alert(year)
+
+console.log(myName)
+
+console.log(secondName)
+
+console.log(myAge)
+
+console.log(myName +  secondName + myAge)
+ */
+
+
+/* let str = "Привіт"
+let num = 123;
+let flag = true;
+let txt = "«true»";
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt) */
+
+/* let number = 42;
+let ryadok = "Це точно РЯДОК"
+let pravda = true;
+let chicla = null;
+let yarik ;
+
+const nemNumber = 22;
+const newRyadok = "РЯДОЧОК"
+const newPravda = false;
+const newChicla = null; */
+
+
+/* const vikMy = 12;
+const vikaboltusa = 13;
+
+console.log(vikMy + vikaboltusa) */
+
+/* let a = 5;
+let b = 10;
+
+let product = a * b;
+
+console.log(product); */
+/* const inputName = prompt("Будь ласка введіть ваше ім'я:")
+console.log("Ваше ім'я: " + userName); */
+/* let inputName = "Так вивчаємо";
+let chatGpt = "Ми вивчаємо чава скрипт"
+confirm(chatGpt)
+alert(inputName) */
+
+if (confirm("Ця дія для компа шкідлива")) {
+    alert("Шо не панятна дякую за підтвердження");
+} 
+else {
+    alert("ДІЮ ВІДМІНЕНО КАКАШКА ТРОЯН ТОП!");
+}
